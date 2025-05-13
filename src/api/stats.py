e81511db5b93e797bbecb8dee126404bf5c5ce74
duartebranco <duartebranco35@gmail.com>
2025-05-13 12:18:29 +0100

    fix requirements

--- src/api/stats.py
+++ src/api/stats.py
@@ -3,7 +3,7 @@ from core.task_manager import TaskManager
 
 stats_bp = Blueprint('stats', __name__)
 
-@stats_bp.route('/stats', methods=['GET'])
+@stats_bp.route('', methods=['GET'])
 def get_stats():
     all_stats = TaskManager.get_all_stats()
     nodes_stats = TaskManager.get_nodes_stats()
